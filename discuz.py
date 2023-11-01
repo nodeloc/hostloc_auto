c5cc05f8e49777be691e8301dcdae9e66fec68a5
James <James@nodeloc.com>
2023-11-01 09:25:52 +0800

    Add chatgpt support.

--- discuz.py
+++ discuz.py
@@ -48,7 +48,7 @@ class Discuz:
 
     def get_reply_tid_list(self):
         tids = []
-        soup = BeautifulSoup(self.go_home(),features="html.parser")
+        soup = BeautifulSoup(self.go_hot(),features="html.parser")
         replys = []
         reply = soup.select_one('.bm_c')
         replys.append(reply)
@@ -69,44 +69,63 @@ class Discuz:
         else :
             logging.error('tid获取失败，退出')
             sys.exit()
-        
 
-    def reply(self,tid,message=''):
-
-        reply_list = ['膜拜神贴，后面的请保持队形~',
-        '啥也不说了，楼主就是给力！',
-        '果断MARK，前十有我必火！',
-        '看了LZ的帖子，我只想说一句很好很强大！',
-        '不错，又占了一个沙发！',
-        '哥顶的不是帖子，是寂寞！',
-        '果断回帖，如果沉了就是我弄沉的很有成就感',
-        '找了很久，终于找到了~',
-        '这个真的很好很不错，我很喜欢~',]
-
-        msg = reply_list[randint(0,len(reply_list)-1)] if message == '' else messages
-        reply_url = f'https://{self.hostname}/forum.php?mod=post&action=reply&tid={tid}&extra=&replysubmit=yes&infloat=yes&handlekey=fastpost&inajax=1'
-        data={
-            'file': '',
-            'message': msg.encode('gbk'),
-            'posttime': int(time()),
-            'formhash': self.formhash,
-            'usesig': 1,
-            'subject': '',
+    def chat_with_gpt(self, prompt):
+        url = "https://api.openai.com/v1/chat/completions"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": self.chatgpt_key
+        }
+        data = {
+            "prompt": prompt,
+            "max_tokens": 50,
+            "temperature": 0.7
         }
 
-        res = self.session.post(reply_url,data=data).text
-        if 'succeed' in res:
-            url = re.search(r'succeedhandle_fastpost\(\'(.+?)\',', res).group(1)
-            logging.info(f'回复发送成功，tid:{tid}，回复:{msg},链接:{"https://"+self.hostname+"/"+url}')
-        else :
-            logging.error('回复发送失败\t'+res)
+        response = requests.post(url, headers=headers, json=data)
+        response_json = response.json()
 
+        if "choices" in response_json:
+            choices = response_json["choices"]
+            if len(choices) > 0 and "text" in choices[0]:
+                return choices[0]["text"]
 
+        return None
+    def reply(self,tid,message=''):
+
+        # 提供一个初始的对话提示
+        prompt = "你好，我是聊天机器人。"
+
+        while True:
+            user_input = input("用户：")
+            prompt += "\n用户：" + user_input
+
+            response = self.chat_with_gpt(prompt)
+            if response:
+                reply_url = f'https://{self.hostname}/forum.php?mod=post&action=reply&tid={tid}&extra=&replysubmit=yes&infloat=yes&handlekey=fastpost&inajax=1'
+                data = {
+                    'file': '',
+                    'message': response,
+                    'posttime': int(time()),
+                    'formhash': self.formhash,
+                    'usesig': 1,
+                    'subject': '',
+                }
+
+                res = self.session.post(reply_url, data=data).text
+                if 'succeed' in res:
+                    url = re.search(r'succeedhandle_fastpost\(\'(.+?)\',', res).group(1)
+                    logging.info(f'回复发送成功，tid:{tid}，回复:{response},链接:{"https://" + self.hostname + "/" + url}')
+                else:
+                    logging.error('回复发送失败\t' + res)
+            else:
+                logging.error('ChatGPT未能成功获取回复\t')
 
 if __name__ == '__main__':
     hostname = 'hostloc.com'
-    username = ''
-    password = ''
+    username = 'NodeLoc'
+    password = 'Acbd1324!@#'
+    chatgpt_key = ''
     discuz = Discuz(hostname,username,password)
     discuz.login()
     discuz.reply(discuz.get_reply_tid())
